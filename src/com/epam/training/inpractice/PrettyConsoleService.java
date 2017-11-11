5bce00f6f1f2e6be4d91345981395cd4f8b87465
Peter Veres2 <Peter_Veres2@epam.com>
2017-11-11 13:20:44 +0100

    Log4j to SLF4J bridge

--- src/com/epam/training/inpractice/PrettyConsoleService.java
+++ src/com/epam/training/inpractice/PrettyConsoleService.java
@@ -2,7 +2,11 @@ package com.epam.training.inpractice;
 
 import java.text.MessageFormat;
 
+import org.apache.log4j.Logger;
+
 public class PrettyConsoleService implements Printer {
+
+    Logger LOGGER = Logger.getLogger(PrettyConsoleService.class);
 
     /*
      * (non-Javadoc)
@@ -11,6 +15,7 @@ public class PrettyConsoleService implements Printer {
      */
     @Override
     public void print(String msg, Object... args) {
+        LOGGER.debug("Now I'm writing to the console...");
         String message = String.format(msg, args);
 
         System.out.print(MessageFormat.format(msg, args));
